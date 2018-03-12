f2154b2a7e8e0e39ed1ba4a05c99c278bda37228
Alex Dolski <alexd@n-ary.net>
2018-03-12 12:16:11 -0500

    Fix ConfigurationFactoryTest not cleaning up after itself

--- src/test/java/edu/illinois/library/metaslurper/config/ConfigurationFactoryTest.java
+++ src/test/java/edu/illinois/library/metaslurper/config/ConfigurationFactoryTest.java
@@ -3,6 +3,7 @@ package edu.illinois.library.metaslurper.config;
 import edu.illinois.library.metaslurper.Application;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,9 +11,19 @@ import static org.junit.Assert.*;
 
 public class ConfigurationFactoryTest {
 
+    private String vmArgValue;
+
     @Before
     public void setUp() {
+        vmArgValue = System.getProperty(Application.CONFIG_VM_ARGUMENT);
         ConfigurationFactory.clearInstance();
+    }
+
+    @After
+    public void tearDown() {
+        if (vmArgValue != null) {
+            System.setProperty(Application.CONFIG_VM_ARGUMENT, vmArgValue);
+        }
     }
 
     @Test
@@ -25,6 +36,7 @@ public class ConfigurationFactoryTest {
 
     @Test
     public void testGetInstanceWithValidConfigFile() {
+
         System.setProperty(Application.CONFIG_VM_ARGUMENT, "./metaslurper.conf.sample");
 
         Configuration config = ConfigurationFactory.getConfiguration();
