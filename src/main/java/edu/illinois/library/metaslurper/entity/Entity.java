ed8411e54e83f37c6c4337a2fcdda04e1dede053
Alex Dolski <alexd@n-ary.net>
2018-05-09 09:32:22 -0500

    Add default implementations for getAccessImageURI() and getMediaType()

--- src/main/java/edu/illinois/library/metaslurper/entity/Entity.java
+++ src/main/java/edu/illinois/library/metaslurper/entity/Entity.java
@@ -5,9 +5,13 @@ import java.util.Set;
 public interface Entity {
 
     /**
+     * This default implementation returns {@literal null}.
+     *
      * @return URI of a high-quality access image. May be {@literal null}.
      */
-    String getAccessImageURI();
+    default String getAccessImageURI() {
+        return null;
+    }
 
     /**
      * @return Elements ascribed to the instance. Element names are arbitrary.
@@ -15,12 +19,16 @@ public interface Entity {
     Set<Element> getElements();
 
     /**
+     * This default implementation returns {@literal null}.
+     *
      * @return IANA media type of the main binary represented by the item.
      *         Should be a specific type and not e.g. {@literal
      *         application/octet-stream} (unless it really is unspecified
      *         binary data). Should be {@literal null} if unknown.
      */
-    String getMediaType();
+    default String getMediaType() {
+        return null;
+    }
 
     /**
      * @return Source service key.
