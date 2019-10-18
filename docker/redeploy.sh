bf089df9bbbeb44f96e0c2bd9fbff6ae5d036009
Alex Dolski <alexd@n-ary.net>
2019-10-18 15:19:49 -0500

    Should have been part of d1ab69c95002af492734c1372ee0d28f5ebb263d

--- docker/redeploy.sh
+++ docker/redeploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]
+then
+    echo "Usage: redeploy.sh <env>"
+    exit 1
+fi
+
 mvn clean package -DskipTests
-docker/docker-build.sh
-docker/ecr-push.sh
+docker/docker-build.sh $1
+docker/ecr-push.sh $1
